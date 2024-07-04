523f4db02c413379422139e05fa4d5266bd2bdc6
gib.son <shai4956@gmail.com>
2024-07-04 19:01:09 +0900

    :sparkles: 할 일 삭제 구현

--- 0704/src/script.js
+++ 0704/src/script.js
@@ -8,8 +8,11 @@ submitBtn.addEventListener("click", addToDoJob);
 function addToDoJob(event) {
     event.preventDefault(); // submit 후 새로고침 방지
 
-    const todoJob = toDoInput.value;
-    console.log(todoJob);
+    const toDoJob = toDoInput.value;
+    if (toDoJob.trim() === "") {
+        console.log("Input is empty");
+        return;
+    }
     
     // li 태그로 할 일 추가
     const li = document.createElement("li");
@@ -20,20 +23,24 @@ function addToDoJob(event) {
 
     const span = document.createElement("span");
     span.classList.add("todo-text");
-    span.textContent = todoJob;
+    span.textContent = toDoJob;
 
     const deleteBtn = document.createElement("button");
     deleteBtn.classList.add("todo-delete");
     deleteBtn.textContent = "X";
+    deleteBtn.onclick = deleteToDoJob;
     
     li.appendChild(checkbox);
     li.appendChild(span);
     li.appendChild(deleteBtn);
 
     toDoList.appendChild(li);
+
+    toDoInput.value = "";
 }
 
-function showToDoJobs() {
-    console.log(jobs);
-    
-}
+function deleteToDoJob(event) {
+    // delete 버튼 눌렀을 때 해당 li 삭제
+    const li = event.target.parentNode;
+    li.remove();
+}  // question: 이벤트리스너와 function의 위치는 어떻게 두어야하는가?
