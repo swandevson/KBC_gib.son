6b47330a2607e423523e392fca60cb280d96091a
gib.son <shai4956@gmail.com>
2024-07-12 19:53:25 +0900

    :sparkels: local storage 기능 추가

--- 0704/src/script.js
+++ 0704/src/script.js
@@ -1,20 +1,35 @@
 const toDoList = document.getElementById("todo-list");
 const toDoInput = document.getElementById("todo-input");
 const submitBtn = document.getElementById("submit-btn");
-let jobs = [];
+
+const jobMap = new Map();
 
 submitBtn.addEventListener("click", addToDoJob);
 
+// Load jobs from local storage on page load
+window.addEventListener("load", loadJobsFromLocalStorage);
+
 function addToDoJob(event) {
-    event.preventDefault(); // submit 후 새로고침 방지
+    event.preventDefault(); // prevent page refresh after submit
 
     const toDoJob = toDoInput.value;
     if (toDoJob.trim() === "") {
         console.log("Input is empty");
         return;
     }
-    
-    // li 태그로 할 일 추가
+
+    const job = {
+        id: Date.now().toString(),
+        text: toDoJob,
+    };
+
+    // Save jobs to local storage
+    saveJobsToLocalStorage(job);
+    addJobToHTML(job);
+}
+
+function addJobToHTML(job) {
+    // Create li element for the job
     const li = document.createElement("li");
     li.classList.add("todo-job");
 
@@ -24,13 +39,14 @@ function addToDoJob(event) {
 
     const span = document.createElement("span");
     span.classList.add("todo-text");
-    span.textContent = toDoJob;
+    span.textContent = job.text;;
 
     const deleteBtn = document.createElement("button");
     deleteBtn.classList.add("todo-delete");
     deleteBtn.textContent = "X";
     deleteBtn.onclick = deleteToDoJob;
-    
+
+    li.dataset.id = job.id;
     li.appendChild(checkbox);
     li.appendChild(span);
     li.appendChild(deleteBtn);
@@ -41,7 +57,43 @@ function addToDoJob(event) {
 }
 
 function deleteToDoJob(event) {
-    // delete 버튼 눌렀을 때 해당 li 삭제
     const li = event.target.parentNode;
     li.remove();
-}  // question: 이벤트리스너와 function의 위치는 어떻게 두어야하는가?
+
+    // Remove job from the jobs array
+    const jobId = li.dataset.id;
+
+    jobMap.delete(jobId);
+
+    // Save updated jobs to local storage
+    updateJobsInLocalStorage();
+}
+
+function saveJobsToLocalStorage(job) {
+    jobMap.set(job.id, job.text);
+    const jobMapArray = Array.from(jobMap.entries());
+    localStorage.setItem("jobs", JSON.stringify(jobMapArray));
+}
+
+function updateJobsInLocalStorage() {
+    const jobMapArray = Array.from(jobMap.entries());
+    localStorage.setItem("jobs", JSON.stringify(jobMapArray));
+}
+
+function loadJobsFromLocalStorage() {
+    let storedJobs = localStorage.getItem("jobs");
+    if(!storedJobs) {
+        return;
+    }
+
+    storedJobs = JSON.parse(storedJobs);
+    storedJobs.forEach(([id, text]) => {
+        const job = {
+            id,
+            text
+        };
+        jobMap.set(id, text);
+        addJobToHTML(job);
+    });
+}
+
